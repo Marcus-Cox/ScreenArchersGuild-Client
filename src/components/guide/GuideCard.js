306ece1524ced27cfc09b9140bf7bf8d5b8e0b87
Marcus-Cox <marcus.cox92@gmail.com>
2022-12-16 11:47:03 -0600

    publishing_date

--- src/components/guide/GuideCard.js
+++ src/components/guide/GuideCard.js
@@ -1,24 +1,31 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./GuideCard.css";
 
 export const GuideCard = ({ guide, delGuide }) => {
 // eslint-disable-next-line
-const navigate  = useNavigate();
 
   return (
     <section key={`guide--${guide.id}`} className="guide">
       
       <img className="guide__image" src={guide.image} />
 
-      <div className="guide__title">Title: {guide.title}</div>
+      <div className="guide__title"> Title: {guide.title}</div>
       
-      <div className="guide__description">
-        
+      <div className="guide__description"> Description:
+      
         {guide.description}
       </div>
 
-      <div className="guide__content">{guide.content}</div>
+      <div className="guide__content"> Content:
+        
+        {guide.content}
+      </div>
+
+      <div className="=guide__publishing_date">
+          Publishing Date:
+        {guide.publishing_date}
+      </div>
 
       <div className="buttons">
         <Link to={`${guide.id}/update`}>
